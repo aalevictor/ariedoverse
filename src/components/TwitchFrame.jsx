8e085569562c6f3dcce725f2c128a9785887646d
Victor Abreu <aalevictor@gmail.com>
2022-10-27 14:50:33 -0300

    .

---
+++ src/components/TwitchFrame.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { TwitchEmbed } from "react-twitch-embed";
+// import ariedo from '../assets/ariedo.png';
+
+function TwitchFrame({ url = 'https://api.twitch.tv/helix/streams', userID = '466120052', token}) {
+    const [live, setLive] = useState(false)
+
+    useEffect(() => {
+        const URI = url + (userID !== '' ? `?user_id=${userID}` : '')
+
+        fetch(URI, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Client-Id': '9q38vwmw39dbre4hzrr9bxx6dgs5n5',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => response.json())
+        .then(
+            data => {
+                console.log(data)
+                setLive(data.data.length > 0)
+            }
+        )
+    }, [ url, token, userID ])
+    
+
+    return (
+        <div className='max-w-[1024px] mx-auto py-8 px-2 max-lg:px-8 text-xl font-black text-offwhite'>
+            { live ? (
+                'Online'
+            ) : (
+                <iframe src="https://player.twitch.tv/?channel=sirariedo&parent=ariedoverse.herokuapp.com" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+            )}
+		</div>
+    )
+}
+
+export default TwitchFrame
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Template from '../components/Template';
+import TwitchFrame from '../components/TwitchFrame';
 
 const Home = () =>{
   return (
     <Template>
-      
+      <TwitchFrame token='yhafdkd42458shmhehxj9gei4502lk' />
     </Template>
   )
 }
